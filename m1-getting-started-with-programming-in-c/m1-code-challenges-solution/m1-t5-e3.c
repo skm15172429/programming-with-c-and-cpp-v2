52d8ae483b092abc10627141534846b07fe85937
skm15172429 <71369268+skm15172429@users.noreply.github.com>
2020-09-16 11:37:44 +0530

    nameage. c

--- m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
+++ m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
@@ -2,8 +2,8 @@
 
     int main() {
 
-        puts("I am Rahul.");
-        puts("I am 20 years old.");
+        printf("I am Rahul.");
+        printf("I am 20 years old.");
 
         return 0;
     }
